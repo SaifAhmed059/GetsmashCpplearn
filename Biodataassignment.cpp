456a134d35e52d1c8775e14092ab10c20b60ac3d
Saif Ahmed <110305537+SaifAhmed059@users.noreply.github.com>
2026-06-26 17:49:24 +0600

    firstdaypracxtice

---
+++ Biodataassignment.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    cout<<"Enter your name:";
+    cin>>name;
+    
+
+    string fathername;
+    cout<<"Enter your father name:";
+    cin>>fathername;
+    
+
+    string mothername;
+    cout<<"Enter your mother name:";
+    cin>>mothername;
+    
+
+    string address;
+    cout<<"Enter your address:";
+    cin>>address;
+    
+
+    cout << name << endl;
+    cout << fathername << endl;
+    cout << mothername << endl;
+    cout << address << endl;
+
+    return 0;
+}
---
+++ program1.cpp
@@ -0,0 +1,22 @@
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     cout << "Hello Coder";
+
+//     return 0;
+// }
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Hello " << name;
+    return 0;
+
+}
