e542c9ef1a29b7f9ee7c8f88d5be8729a8db0655
Saif Ahmed <110305537+SaifAhmed059@users.noreply.github.com>
2026-06-28 17:22:03 +0600

    for loop operartions

---
+++ ForLoop.cpp
@@ -0,0 +1,18 @@
+// for(initialization; condition; increment/decrement)
+// {
+//     // Code to be executed in each iteration
+// }
+
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    for(int i = 1; i <= 5; i++)
+    {
+        cout << i << endl;
+    }
+
+    return 0;
+}
