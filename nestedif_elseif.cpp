11a66ff319b22b714e26eb8285d60d047a4ff93d
Saif Ahmed <110305537+SaifAhmed059@users.noreply.github.com>
2026-06-28 16:50:26 +0600

    another practice

--- nestedif_elseif.cpp
+++ nestedif_elseif.cpp
@@ -1,16 +1,32 @@
 #include<iostream>
 using namespace std;
 
+// int main()
+// {
+//     int marks;
+//     cout << "Enter your marks: ";
+//     cin >> marks;
+
+//         if (marks >= 90)
+//         cout << "Eligible for Engineering";
+//         else
+//         cout << "Not Eligible for Engineering";
+
+//     return 0;    
+// }
+
+
 int main()
 {
     int marks;
-    cout << "Enter your marks: ";
+    cout << "Enter your Commerce & Accounts Marks:";
     cin >> marks;
 
-        if (marks >= 90)
-        cout << "Eligible for Engineering";
-        else
-        cout << "Not Eligible for Engineering";
+    if (marks > 90)
+    cout << "Eligible for B.Com(Hons)";
+    else
+    cout << "Not Eligible for B.Com(Hons)";
 
-    return 0;    
+
+    return 0;
 }
