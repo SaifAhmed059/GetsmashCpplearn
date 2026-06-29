dbda88f686ad08fbc56b09b283affc94dfb15b53
Saif Ahmed <110305537+SaifAhmed059@users.noreply.github.com>
2026-06-29 16:25:30 +0600

    function practice

---
+++ function.cpp
@@ -0,0 +1,26 @@
+// #include<iostream>
+// using namespace std;
+
+// int add(int a, int b){
+//     return a + b;
+// }
+
+// int main(){
+//     int sum = add(5, 3);
+//     cout << sum << endl;
+//     return 0;
+// }
+
+
+#include<iostream>
+using namespace std;
+
+void fun1(){
+    cout<< " Saif Ahmed " << endl;
+}
+
+int main(){
+    fun1();
+
+    return 0;
+}
