06b90af62b48544afd48d518fa3480fac18a7a6f
Saif Ahmed <110305537+SaifAhmed059@users.noreply.github.com>
2026-06-30 17:31:15 +0600

    another practice of pointer

--- pointer.cpp
+++ pointer.cpp
@@ -2,12 +2,17 @@
 using namespace std;
 
 int main(){
-    int a = 10;
-    int *p = &a;
-    cout <<a << endl;
-    cout <<&a << endl;
-    cout << p << endl;
-    cout << *p << endl;
+    // int a = 10;
+    // int *p = &a;
+    // cout <<a << endl;
+    // cout <<&a << endl;
+    // cout << p << endl;
+    // cout << *p << endl;
+
+    int x = 15;
+    int *p=&x;
+    *p=*p+1;
+    cout<<x<<endl;
 
 
     return 0;
